3d34bea0d743d3f465e476564df32be8033c28d0
ShajeedHossain <shakun650@gmail.com>
2026-01-05 20:40:28 +0600

    Added django-filter dependency and created accesslog viewset with crud operations

--- access_control/views.py
+++ access_control/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status, viewsets
+from rest_framework.response import Response
+
+from .models import AccessLog
+from .serializers import AccessLogSerializer
+
 
 # Create your views here.
+class AccessLogViewSet(viewsets.ModelViewSet):
+    queryset = AccessLog.objects.all()
+    serializer_class = AccessLogSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['card_id', 'door_name', 'access_granted']
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- access_control_system/settings.py
+++ access_control_system/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework', 
     'access_control', 
+    'django_filters',
 ]
 
 MIDDLEWARE = [

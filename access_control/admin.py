62237ba1c37f84c56c21f96a277e85fe4e78a1bf
ShajeedHossain <shakun650@gmail.com>
2026-01-06 21:11:25 +0600

    Added django admin UI

--- access_control/admin.py
+++ access_control/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import AccessLog
+
 # Register your models here.
+
+
+@admin.register(AccessLog)
+class AccessLogAdmin(admin.ModelAdmin):
+    list_display = ['id', 'card_id', 'door_name', 'access_granted', 'timestamp']
+    list_filter = ['access_granted', 'door_name', 'timestamp']
+    search_fields = ['card_id', 'door_name']
+    readonly_fields = ['timestamp']
